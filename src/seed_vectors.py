020be056c2a6c1502fd3dfcee92e154c2c99d12e
Raashil Aadhyanth <raashil.aadhyanth@gmail.com>
2024-08-09 12:58:30 -0400

    Refactored

--- src/seed_vectors.py
+++ src/seed_vectors.py
@@ -5,6 +5,7 @@ from face_api import get_image_vector
 import os
 import tensorflow as tf
 import numpy as np
+import shutil
 
 print(tf.__version__)
 
@@ -15,7 +16,8 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # Path to the folder containing images
-image_folder = '../data/dummy_dataset/input_images'
+image_folder = '../data/dummy_dataset/images_to_vectorize'
+destination_folder = '../data/dummy_dataset/input_images'
 
 # Reference to your Firestore collection
 collection_ref = db.collection('vectors')
@@ -23,6 +25,7 @@ collection_ref = db.collection('vectors')
 # Loop through each image in the folder
 for image_name in os.listdir(image_folder):
     image_path = os.path.join(image_folder, image_name)
+    print('image_path', image_path)
 
     # Get the image vector
     image_vector = get_image_vector(image_path)
@@ -40,5 +43,8 @@ for image_name in os.listdir(image_folder):
 
     # Add the data to Firestore
     collection_ref.document(unique_id).set(data)
+
+    # Move the processed image to the destination folder
+    shutil.move(image_path, os.path.join(destination_folder, image_name))
 
 print("All images have been processed and stored in Firestore.")
